b03780b7d1ab149a7f5634dfe228503ba502ac65
Pooja Patil <poojapatil15295@gmail.com>
2022-12-25 16:45:42 +0530

    Added instructions

--- src/components/LoadTest.js
+++ src/components/LoadTest.js
@@ -241,13 +241,12 @@ export default function LoadTest() {
       ) : (
         <div className="inst border text-center  ">
           <h3> Instructions :-</h3>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta
-            soluta eveniet enim? Impedit, expedita delectus laboriosam
-            consequuntur molestiae voluptate, maiores architecto, ipsum aut
-            accusantium minus voluptatibus repellendus. Dolorem, illo
-            consequuntur.
-          </p>
+          <p>1. All questions are compulsary.</p>
+          
+          <p>2. Use pen for rough work.</p>
+          
+          <p>3. Make sure you clicked on submit button to successfully complete the test.</p>
+          
           <button
             className="btn btn-info mx-2 my--2"
             onClick={NextQuestionfrist}
